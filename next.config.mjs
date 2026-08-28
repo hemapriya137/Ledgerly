c8fa9ec16645264abf2876745a3a7152ab511ab3
hemapriya137 <hemapriyadharshinih@gmail.com>
2026-08-28 19:18:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} /*/
 const nextConfig = {
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
   eslint: {
